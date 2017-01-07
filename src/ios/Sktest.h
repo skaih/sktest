54124470ff0a19ce8e91ac127f3d1d02b5a3a8a5
sk <skaih@qq.com>
2017-01-07 17:57:44 +0800

    2

--- src/ios/Sktest.h
+++ src/ios/Sktest.h
@@ -1,6 +1,7 @@
 /********* TestPlugin.h Cordova Plugin Header *******/
 
 #import <Cordova/CDV.h>
+#define loginToken @"loginToken"
 @interface Sktest : CDVPlugin
 - (void)test:(CDVInvokedUrlCommand*)command;
     @end
