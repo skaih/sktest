4f3cc7649cd80aa75bc49e856e457c73e246c7aa
sk <skaih@qq.com>
2017-01-07 17:51:20 +0800

    1

--- src/ios/Sktest.m
+++ src/ios/Sktest.m
@@ -2,12 +2,30 @@
 
 #import "Sktest.h"
 #import <Cordova/CDV.h>
-
+#import "DCore.h"
 @implementation Sktest
 - (void)test:(CDVInvokedUrlCommand*)command
     {
         UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"标题" message:@"你好世界！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
         [alertview show];
-    }
-    
+		
+		
+		[DCore al:@"first"];
+    NSString* action = [command.arguments objectAtIndex:0];
+	[DCore al:action];
+	NSString* str = [command.arguments objectAtIndex:1];
+	if([action isEqualToString:@"al"]){ 
+	  [DCore al:str];
+	}else if([action isEqualToString:@"loginsave"]){ 
+	  NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+       [standardDefaults setObject:str forKey:loginToken];
+       [standardDefaults synchronize];
+	}else if([action isEqualToString:@"getLoginToken"]){ 
+	  NSUserDefaults *standardDefaults = [NSUserDefaults standardUserDefaults];
+      NSString *token = [standardDefaults stringForKey:loginToken];
+	  //CDVPluginResult *result;
+      //result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:token]; 
+      //[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+	}
+    } 
     @end
