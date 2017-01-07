47f322f0d5380371e058836ab6f1a4bd1e249fba
sk <skaih@qq.com>
2017-01-07 11:47:28 +0800

    122

---
+++ src/ios/Sktest.h
@@ -0,0 +1,6 @@
+/********* TestPlugin.h Cordova Plugin Header *******/
+
+#import <Cordova/CDV.h>
+@interface Sktest : CDVPlugin
+- (void)test:(CDVInvokedUrlCommand*)command;
+    @end
---
+++ src/ios/Sktest.m
@@ -0,0 +1,13 @@
+﻿/***************** TestPlugin.m ********************/
+
+#import "Sktest.h"
+#import <Cordova/CDV.h>
+
+@implementation Sktest
+- (void)test:(CDVInvokedUrlCommand*)command
+    {
+        UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"标题" message:@"你好世界！" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+        [alertview show];
+    }
+    
+    @end
